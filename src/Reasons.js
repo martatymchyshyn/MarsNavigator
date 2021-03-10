f4deb6942ca1351347c7fb6cfe55b0e71ae48cfa
martatymchyshyn <55658484+martatymchyshyn@users.noreply.github.com>
2021-03-10 16:18:11 +0200

    Added photos

--- src/Reasons.js
+++ src/Reasons.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from "@material-ui/core/Typography";
-import "./styles/reasons.sass"
-
+import "./styles/reasons.sass";
+import analysis from "./img/analysis.png";
+import experience from "./img/experience.png";
+import leader from "./img/leader.png";
 
 function Reasons() {
     return (
@@ -17,8 +18,8 @@ function Reasons() {
                         <div className="item">
                             <img
                                 className="image"
-                                src= "https://www.flaticon.com/svg/vstatic/svg/124/124633.svg?token=exp=1613469086~hmac=cd4aea02ea32b34415eb7e5273ea3256"
-                                alt="suitcase"
+                                src= {analysis}
+                                alt="analysis"
                             />
                             <Typography variant="h6" className="title">
                                 Strategic
@@ -33,7 +34,7 @@ function Reasons() {
                         <div className="item">
                             <img
                                 className="image"
-                                src="https://www.flaticon.com/svg/vstatic/svg/3885/3885864.svg?token=exp=1613469224~hmac=be1abc6705a722f6f2d7f51e53db5e76"
+                                src= {experience}
                                 alt="graph"
                             />
                             <Typography variant="h6" className="title">
@@ -50,7 +51,7 @@ function Reasons() {
                         <div className="item">
                             <img
                                 className="image"
-                                src="https://www.flaticon.com/svg/vstatic/svg/3050/3050413.svg?token=exp=1613469294~hmac=34aeb2e238cd59ccc00bf3b1b38be932"
+                                src= {leader}
                                 alt="clock"
                             />
                             <Typography variant="h6" className="title">
